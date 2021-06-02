00b36f1e7bdb100286e3c8f0983d0c2b234574fe
Gabriele Muratori <55052791+gmurrt@users.noreply.github.com>
2021-06-03 00:04:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    function calculate(fn) {
+                        return new Function('return ' + fn)();
+                      }
+                      
+                      display.innerText = calculate(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
